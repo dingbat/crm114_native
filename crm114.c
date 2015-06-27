5689c91e87dbe0bf6f25695e7a3de6385fc2b69d
Dan Hassin <danhassin@mac.com>
2015-06-28 01:30:54 +0200

    Combine into one class

--- crm114.c
+++ crm114.c
@@ -6,166 +6,175 @@
 #include "crm114_lib.h"
 #include "crm114_internal.h"
 
-static VALUE rb_dbClass;
+typedef struct CRM114
+{
+  CRM114_CONTROLBLOCK *cb;
+  CRM114_DATABLOCK *db;
+  CRM114_ERR err;
+} CRM114;
 
-void Init_ControlBlock(VALUE module);
-void Init_DataBlock(VALUE module);
-void _db_set(VALUE obj, CRM114_DATABLOCK *db);
+static VALUE crm_getClasses(VALUE obj);
+static VALUE crm_setClasses(VALUE obj, VALUE rb_classes_ary);
+static VALUE crm_getDatablockSize(VALUE obj);
+static VALUE crm_setDatablockSize(VALUE obj, VALUE rb_size);
+static VALUE crm_setRegex(VALUE obj, VALUE regex);
+static VALUE crm_setPipeline(VALUE obj);
+
+static VALUE crm_config(VALUE obj);
+static VALUE crm_learn_text(VALUE obj, VALUE whichClass, VALUE text);
+
+static VALUE crm_alloc(VALUE klass);
+static VALUE crm_init(VALUE obj, VALUE flags);
+static void crm_mark(CRM114 *crm);
+static void crm_free(CRM114 *crm);
 
 void Init_CRM114()
 {
-  VALUE module = rb_define_module("CRM114");
+  VALUE crm114_class = rb_define_class("CRM114", rb_cObject);
+  rb_define_alloc_func(crm114_class, crm_alloc);
+  rb_define_method(crm114_class, "initialize", crm_init, 1);
 
-  Init_DataBlock(module);
-  Init_ControlBlock(module);
+  // Virtual attrs
+  rb_define_method(crm114_class, "classes", crm_getClasses, 0);
+  rb_define_method(crm114_class, "classes=", crm_setClasses, 1);
+  rb_define_method(crm114_class, "datablock_size", crm_getDatablockSize, 0);
+  rb_define_method(crm114_class, "datablock_size=", crm_setDatablockSize, 1);
+  rb_define_method(crm114_class, "regex=", crm_setRegex, 1);
+  rb_define_method(crm114_class, "pipeline=", crm_setPipeline, 0); //TODO
+
+  // Methods
+  rb_define_method(crm114_class, "config", crm_config, 0);
+  rb_define_method(crm114_class, "learn_text", crm_learn_text, 2);
+
+  // Constants
+  rb_define_const(crm114_class, "OSB", LONG2FIX(CRM114_OSB));
+  rb_define_const(crm114_class, "SVM", LONG2FIX(CRM114_SVM));
+  rb_define_const(crm114_class, "FSCM", LONG2FIX(CRM114_FSCM));
+  rb_define_const(crm114_class, "HYPERSPACE", LONG2FIX(CRM114_HYPERSPACE));
+  rb_define_const(crm114_class, "ENTROPY", LONG2FIX(CRM114_ENTROPY));
+  rb_define_const(crm114_class, "STRING", LONG2FIX(CRM114_STRING));
+  rb_define_const(crm114_class, "UNIQUE", LONG2FIX(CRM114_UNIQUE));
+  rb_define_const(crm114_class, "CROSSLINK", LONG2FIX(CRM114_CROSSLINK));  
 }
 
-///////////////////////////////
-//
-//  ControlBlock
-//
-///////////////////////////////
+static VALUE crm_alloc(VALUE klass)
+{
+  return Data_Wrap_Struct(klass, crm_mark, crm_free, NULL);
+}
 
-static VALUE cb_setClasses(VALUE obj, VALUE rb_classes_ary);
-static VALUE cb_setDatablockSize(VALUE obj, VALUE rb_size);
-
-static void cb_mark(CRM114_CONTROLBLOCK *crm)
+static void crm_mark(CRM114 *crm)
 {
 }
 
-static void cb_free(CRM114_CONTROLBLOCK *crm)
+static void crm_free(CRM114 *crm)
 {
 }
 
-static VALUE cb_alloc(VALUE klass)
+/////
+
+static VALUE crm_init(VALUE obj, VALUE flags)
 {
-  return Data_Wrap_Struct(klass, cb_mark, cb_free, NULL);
-}
+  CRM114 *crm = malloc(sizeof(CRM114));
+  crm->cb = crm114_new_cb();
 
-static VALUE cb_init(VALUE obj, VALUE flags, VALUE classes, VALUE datablock_size)
-{
-  CRM114_CONTROLBLOCK *cb = crm114_new_cb();
+  DATA_PTR(obj) = crm;
 
-  DATA_PTR(obj) = cb;
-
-  crm114_cb_setflags(cb, FIX2INT(flags));
-  crm114_cb_setclassdefaults(cb);
-  cb_setClasses(obj, classes);
-  cb_setDatablockSize(obj, datablock_size);
-  crm114_cb_setblockdefaults(cb);
+  crm114_cb_setflags(crm->cb, FIX2LONG(flags));
+  crm114_cb_setclassdefaults(crm->cb);
 
   return Qnil;
 }
 
-static VALUE cb_setClasses(VALUE obj, VALUE rb_classes_ary)
+static VALUE crm_config(VALUE obj)
 {
-  CRM114_CONTROLBLOCK *cb = DATA_PTR(obj);
+  CRM114 *crm = DATA_PTR(obj);
+  
+  rb_yield(obj);
+  crm114_cb_setblockdefaults(crm->cb);
+
+  return Qnil;
+}
+
+static VALUE crm_setClasses(VALUE obj, VALUE rb_classes_ary)
+{
+  CRM114 *crm = DATA_PTR(obj);
 
   int length = RARRAY_LEN(rb_classes_ary);
+  crm->cb->how_many_classes = length;
   for (int i = 0; i < length; i++) {
     char *name = RSTRING_PTR(rb_ary_entry(rb_classes_ary, i));
-    strcpy(cb->class[i].name, name);
+    strcpy(crm->cb->class[i].name, name);
   }
-  cb->how_many_classes = length;
 
   return rb_classes_ary;
 }
 
-static VALUE cb_getClasses(VALUE obj)
+static VALUE crm_getClasses(VALUE obj)
 {
-  CRM114_CONTROLBLOCK *cb = DATA_PTR(obj);
+  CRM114 *crm = DATA_PTR(obj);
 
-  int length = cb->how_many_classes;
+  int length = crm->cb->how_many_classes;
   VALUE ary = rb_ary_new2(length);
   for (int i = 0; i < length; i++) {
-    rb_ary_store(ary, i, rb_str_new_cstr(cb->class[i].name));
+    rb_ary_store(ary, i, rb_str_new_cstr(crm->cb->class[i].name));
   }
 
   return ary;
 }
 
-static VALUE cb_setDatablockSize(VALUE obj, VALUE rb_size)
+static VALUE crm_setDatablockSize(VALUE obj, VALUE rb_size)
 {
-  CRM114_CONTROLBLOCK *cb = DATA_PTR(obj);
-  cb->datablock_size = FIX2INT(rb_size);
+  CRM114 *crm = DATA_PTR(obj);
+  crm->cb->datablock_size = FIX2INT(rb_size);
   return rb_size;
 }
 
-static VALUE cb_getDatablockSize(VALUE obj)
+static VALUE crm_getDatablockSize(VALUE obj)
 {
-  CRM114_CONTROLBLOCK *cb = DATA_PTR(obj);
-  return INT2FIX(cb->datablock_size);
+  CRM114 *crm = DATA_PTR(obj);
+  return INT2FIX(crm->cb->datablock_size);
 }
 
-static VALUE cb_new_db(VALUE obj)
+static VALUE crm_setRegex(VALUE obj, VALUE regex)
 {
-  CRM114_CONTROLBLOCK *cb = DATA_PTR(obj);
-  CRM114_DATABLOCK *db = crm114_new_db(cb);
-
-  VALUE klass = rb_dbClass;//rb_const_get(rb_cObject, rb_intern("CRM114::DataBlock"));  
-  VALUE rb_db = rb_funcall(klass, rb_intern("new"), 0);
-  _db_set(rb_db, db);
-
-  return rb_db;
+  CRM114 *crm = DATA_PTR(obj);
+  
+  char *regex_str = RSTRING_PTR(regex);
+  int regex_len = RSTRING_LEN(regex);
+  crm114_cb_setregex(crm->cb, regex_str, regex_len);
+  
+  return regex;
 }
 
-void Init_ControlBlock(VALUE module)
+static VALUE crm_setPipeline(VALUE obj)
 {
-  VALUE rb_controlBlock = rb_define_class_under(module, "ControlBlock", rb_cObject);
-  rb_define_alloc_func(rb_controlBlock, cb_alloc);
-  rb_define_method(rb_controlBlock, "initialize", cb_init, 3);
+  //TODO
 
-  // Virtual attrs
-  rb_define_method(rb_controlBlock, "classes", cb_getClasses, 0);
-  rb_define_method(rb_controlBlock, "datablock_size", cb_getDatablockSize, 0);
-
-  // Methods
-  rb_define_method(rb_controlBlock, "new_db", cb_new_db, 0);
-
-  // Constants
-  VALUE rb_classifier_flags = rb_define_module_under(module, "Classifier");
-  rb_define_const(rb_classifier_flags, "OSB", INT2FIX(CRM114_OSB));
-  rb_define_const(rb_classifier_flags, "SVM", INT2FIX(CRM114_SVM));
-  rb_define_const(rb_classifier_flags, "FSCM", INT2FIX(CRM114_FSCM));
-  rb_define_const(rb_classifier_flags, "HYPERSPACE", INT2FIX(CRM114_HYPERSPACE));
-  rb_define_const(rb_classifier_flags, "ENTROPY", INT2FIX(CRM114_ENTROPY));
-  rb_define_const(rb_classifier_flags, "STRING", INT2FIX(CRM114_STRING));
-  rb_define_const(rb_classifier_flags, "UNIQUE", INT2FIX(CRM114_UNIQUE));
-  rb_define_const(rb_classifier_flags, "CROSSLINK", INT2FIX(CRM114_CROSSLINK));  
-}
-
-///////////////////////////////
-//
-//  DataBlock
-//
-///////////////////////////////
-
-static void db_mark(CRM114_CONTROLBLOCK *crm)
-{
-}
-
-static void db_free(CRM114_CONTROLBLOCK *crm)
-{
-}
-
-static VALUE db_alloc(VALUE klass)
-{
-  return Data_Wrap_Struct(klass, db_mark, db_free, NULL);
-}
-
-void _db_set(VALUE obj, CRM114_DATABLOCK *db)
-{
-  DATA_PTR(obj) = db;
-}
-
-VALUE db_init(VALUE obj)
-{
   return Qnil;
 }
 
-void Init_DataBlock(VALUE module)
+VALUE crm_learn_text(VALUE obj, VALUE whichClass, VALUE text)
 {
-  rb_dbClass = rb_define_class_under(module, "DataBlock", rb_cObject);
-  rb_define_alloc_func(rb_dbClass, db_alloc);
-  rb_define_method(rb_dbClass, "initialize", db_init, 0);
+  CRM114 *crm = DATA_PTR(obj);
+
+  int idx;
+  if (TYPE(whichClass) == T_STRING) {
+    //get the index of the string
+    char *string = RSTRING_PTR(whichClass);
+    int length = crm->cb->how_many_classes;
+    for (idx = 0; idx < length; idx++) {
+      if (strcmp(crm->cb->class[idx].name, string) == 0) {
+        break;
+      }
+    }
+  } else {
+    idx = FIX2INT(whichClass);
+  }
+
+  char *text_str = RSTRING_PTR(text);
+  int text_len = RSTRING_LEN(text);
+
+  crm114_learn_text(&(crm->db), idx, text_str, text_len);
+
+  return Qnil;
 }
--- tests/control_block_test.rb
+++ tests/control_block_test.rb
@@ -4,15 +4,13 @@ require_relative '../CRM114'
 
 class ControlBlockTest < Test::Unit::TestCase
   def test_classes
-    cb = CRM114::ControlBlock.new(0, ["a", "b", "c"], 8000)
+    cb = CRM114.new(CRM114::SVM | CRM114::STRING)
+    cb.config do |config|
+      config.classes = ["a","b","c"]
+    end
     assert_equal ["a","b","c"], cb.classes
   end
 
   def test_db
-    cb = CRM114::ControlBlock.new(0, ["a", "b", "c"], 8000)
-    db = cb.new_db
-    assert_equal CRM114::DataBlock, db.class
-
-    
   end
 end
