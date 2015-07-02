139bf027a44e280461d3e3cdfdb22165b337d551
Dan Hassin <danhassin@mac.com>
2015-07-02 12:32:30 +0200

    Try linking to libcrm114 with local path

--- ext/crm114_native/extconf.rb
+++ ext/crm114_native/extconf.rb
@@ -9,7 +9,7 @@ have_library("tre") or raise
 
 dir_config("crm114", File.join(path,'include'), path)
 
-$LOCAL_LIBS << ' -lcrm114'
+$LOCAL_LIBS << " #{File.join(path,'libcrm114.a')}"
 $CFLAGS << ' -std=c99'
 
 create_makefile("crm114_native")
